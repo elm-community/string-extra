1c10e1a9a1b2a0f06d2565bc90c4b743b7c5ae54
Tad Lispy <tadeusz@lazurski.pl>
2020-07-15 20:52:26 +0200

    Add support of Latin-Extended-A to removeAccents
    I think characters from other Unicode blocks should also be added for
    completeness, but I lack required linguistic knowledge to provide
    translations. Hopefully we can consider it a small progress :)

--- tests/RemoveAccentsTest.elm
+++ tests/RemoveAccentsTest.elm
@@ -10,10 +10,14 @@ removeAccentsTest =
     describe "removeAccents"
         [ test "Should result string without accents" <|
             \() ->
-                removeAccents "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ"
-                    |> Expect.equal "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC"
+                removeAccents "ąáàãâäćęéèêëíìîïłóòõôöśúùûüçźżĄÁÀÃÂÄĆĘÉÈÊËÍÌÎÏŁÓÒÕÖÔŚÚÙÛÜÇŹŻ"
+                    |> Expect.equal "aaaaaaceeeeeiiiilooooosuuuuczzAAAAAACEEEEEIIIILOOOOOSUUUUCZZ"
         , test "Should result in phrase without accents" <|
             \() ->
                 removeAccents "andré JOÂO"
                     |> Expect.equal "andre JOAO"
+        , test "Should produce a Polish phrase without accents" <|
+            \() ->
+                removeAccents "Cześć! Jak się masz? Śmiało usuń akcenty!"
+                    |> Expect.equal "Czesc! Jak sie masz? Smialo usun akcenty!"
         ]
